71a1f8d390d6b01c84210e158fc8f57004f3474b
Helder Ferreira <helder.ferreira@pingpost.pt>
2018-06-04 16:15:11 +0100

    -> Conditionals in vuejs

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 new Vue({
     el: '#vue-app',
     data: {
-        available: false,
-        nearby: false
+        error: false,
+        success: false
     },
     methods: {
-        toggleAvailableState: function () {
-            this.available = !this.available;
-        }
+
     },
     computed: {
         /* Computed properties are cached based on their dependencies, it's only fired when at least one of this internal dependencies change */
-        compClasses: function () {
-            return {
-                available: this.available,
-                nearby: this.nearby
-            };
-        }
+
     }
 });
