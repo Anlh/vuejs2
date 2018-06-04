7f70c47edf167e1f1b2f3dec06757175d0a92457
Helder Ferreira <helder.ferreira@pingpost.pt>
2018-06-04 15:13:04 +0100

    -> Added a more intuitive comment line on the computed property from Vue

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ new Vue({
             return this.age + this.b;
         }*/
     },
-    computed: { /* The method is only fired when their internal dependencies change */
+    computed: {
+        /* Computed properties are cached based on their dependencies, it's only fired when at least one of this internal dependencies change */
         addToA: function () {
-            console.log('addToA');
+            console.log(this.age++);
             return this.age + this.a;
         },
         addToB: function () {
