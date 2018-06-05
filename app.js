50ac162b09f98d6ad8d6ab3bafc547eb453b80ad
Helder Ferreira <helder.ferreira@pingpost.pt>
2018-06-05 13:45:56 +0100

    -> Created branch multiple instance in vuejs

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-new Vue({
-    el: '#vue-app',
+var one = new Vue({
+    el: '#vue-app-one',
     data: {
+        title: 'Vue app one'
+    },
+    methods: {},
+    computed: {
+        /* Computed properties are cached based on their dependencies, it's only fired when at least one of this internal dependencies change */
+        greet: function () {
+            return 'Hello from app one';
+        }
+    }
+});
 
+var two = new Vue({
+    el: '#vue-app-two',
+    data: {
+        title: 'Vue app two'
     },
     methods: {
-
+        changeTheTitle: function () {
+            one.title = 'Title has changed';
+        }
     },
     computed: {
         /* Computed properties are cached based on their dependencies, it's only fired when at least one of this internal dependencies change */
-
+        greet: function () {
+            return 'Hello from app two';
+        }
     }
 });
