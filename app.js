1e4cf3caf30a6d3e4ffaf1289bcfc5c8ee57b28f
Helder Ferreira <helder.ferreira@pingpost.pt>
2018-06-05 14:59:29 +0100

    -> Created branch for the intro to components from vuejs

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+// If we want to share the same data reference through all the greeting components we need to declare and assign variable like this
+// and pass to the return of the data function
+// If we used this approach instead what javascript will do under the hood was create a single reference in memory to use in all the greeting components created
+// Will could cause some side effects, like if we would update anything from data, that data mutation would be propagated to all the instances of greeting
+/*var data = {
+    name: 'helder'
+};*/
+
+Vue.component('greeting', {
+    template: '<p v-on:click="changeTheName">Hey there, I am {{name}}</p>',
+    data: function () {
+        return { // This will store a new copy of this returned object for each closure created
+            name: 'Helder'
+        }
+    },
+    methods: {
+        changeTheName: function () {
+            this.name = 'Tiago'
+        }
+    }
+});
+
 var one = new Vue({
     el: '#vue-app-one',
     data: {
@@ -6,9 +28,7 @@ var one = new Vue({
     methods: {},
     computed: {
         /* Computed properties are cached based on their dependencies, it's only fired when at least one of this internal dependencies change */
-        greet: function () {
-            return 'Hello from app one';
-        }
+
     }
 });
 
@@ -17,15 +37,9 @@ var two = new Vue({
     data: {
         title: 'Vue app two'
     },
-    methods: {
-        changeTheTitle: function () {
-            one.title = 'Title has changed';
-        }
-    },
+    methods: {},
     computed: {
         /* Computed properties are cached based on their dependencies, it's only fired when at least one of this internal dependencies change */
-        greet: function () {
-            return 'Hello from app two';
-        }
+
     }
 });
