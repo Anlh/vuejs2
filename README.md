8ec5bf76c67a00cb35eeee8810907379128e737f
Helder Ferreira <helder.ferreira@pingpost.pt>
2018-06-05 15:15:21 +0100

    -> Created branch for the refs feature in vuejs

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1>Refs in vuejs</h1>
+<p>This is useful to access the properties of dom nodes</p>
--- app.js
+++ app.js
@@ -1,45 +1,13 @@
-// If we want to share the same data reference through all the greeting components we need to declare and assign variable like this
-// and pass to the return of the data function
-// If we used this approach instead what javascript will do under the hood was create a single reference in memory to use in all the greeting components created
-// Will could cause some side effects, like if we would update anything from data, that data mutation would be propagated to all the instances of greeting
-/*var data = {
-    name: 'helder'
-};*/
-
-Vue.component('greeting', {
-    template: '<p v-on:click="changeTheName">Hey there, I am {{name}}</p>',
-    data: function () {
-        return { // This will store a new copy of this returned object for each closure created
-            name: 'Helder'
-        }
+new Vue({
+    el: '#vue-app',
+    data: {
+        title: 'Vue app',
+        output: ''
     },
     methods: {
-        changeTheName: function () {
-            this.name = 'Tiago'
+        submitValue: function () {
+            console.log(this.$refs);
+            this.output = this.$refs.input.value;
         }
-    }
-});
-
-var one = new Vue({
-    el: '#vue-app-one',
-    data: {
-        title: 'Vue app one'
-    },
-    methods: {},
-    computed: {
-        /* Computed properties are cached based on their dependencies, it's only fired when at least one of this internal dependencies change */
-
-    }
-});
-
-var two = new Vue({
-    el: '#vue-app-two',
-    data: {
-        title: 'Vue app two'
-    },
-    methods: {},
-    computed: {
-        /* Computed properties are cached based on their dependencies, it's only fired when at least one of this internal dependencies change */
-
     }
 });
